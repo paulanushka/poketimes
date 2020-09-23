0371734f321f4eb4b6fa111b1f8ef7294a935c56
Anushka Paul <anushkapaul1103@gmail.com>
2020-09-24 01:51:34 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+const About=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue magna sit amet metus lobortis, quis commodo purus malesuada. Suspendisse id metus quam. Nunc eu tempus mauris. Nunc rhoncus suscipit tempus. Pellentesque auctor nunc eu fringilla imperdiet. Sed dignissim mauris et mattis fringilla. Vestibulum vitae ante ultricies, accumsan lectus vel, venenatis ipsum. Phasellus magna ante, pretium et nisi sed, maximus pretium diam. Phasellus ultricies rutrum elit non laoreet. Donec dignissim tortor eu est varius ultrices. Sed sed nisl efficitur, ullamcorper enim vitae, finibus mauris. Praesent ac venenatis neque, in finibus lectus. Aenean elementum tincidunt sagittis. Nullam lobortis arcu lacinia, ultrices felis non, euismod ex.</p>
+        </div>
+    )
+}
+export default Rainbow(About);
